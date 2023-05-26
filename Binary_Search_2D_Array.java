686a125b65e00e9b1ed2127e73673d54cf9e5dc4
Haresh <109500895+Hareshku@users.noreply.github.com>
2023-05-26 23:06:23 +0500

    Update Binary_Search_2D_Array.java

--- Binary_Search_2D_Array.java
+++ Binary_Search_2D_Array.java
@@ -19,7 +19,7 @@ class Arrays{
                 l=mid-1;
              }
         }
-        return-f-1;
+        return-f;
     }
     public static void main(String[] args) {
         int [][] ar={{1,2,3,4},{5,6,7,8}}; 
